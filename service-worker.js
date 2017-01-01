4c9cb31036d8706453e7546180b18f16b7f718ed
EriveltonAlandelon <eriveltonalandelon@gmail.com>
2017-01-01 06:27:33 -0200

    Testes

--- service-worker.js
+++ service-worker.js
@@ -1,73 +1,64 @@
-'use strict';
 
-const PREFIX = 'erivelton';
-const HASH = '424c85f60'; // Calculated when running `gulp`.
-const OFFLINE_CACHE = `${ PREFIX }-${ HASH }`;
-const OFFLINE_URL = '/';
+var CACHE = 'cache-and-update';
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(OFFLINE_CACHE).then(function(cache) {
-      return cache.addAll([
-        './fonts/proximanova-bold-webfont.eot',
-        './fonts/proximanova-bold-webfont.ttf',
-        './fonts/proximanova-bold-webfont.woff',
-        './fonts/proximanova-light-webfont.eot',
-        './fonts/proximanova-light-webfont.ttf',
-        './fonts/proximanova-light-webfont.woff',
-        './fonts/proximanova-regular-webfont.eot',
-        './fonts/proximanova-regular-webfont.ttf',
-        './fonts/proximanova-regular-webfont.woff',
-        './index.html',
-        './js/nprogress.min.js',
-        './img/facebook-icon.png',
-        './img/autor.jpg',
-        './img/play.jpg',
-        './img/fifa08.jpg',
-        './img/potplayer.jpg',
-        './img/handycache.jpg',
-        './img/pes2015.jpg',
-        './js/app.js'
-      ]);
-    })
-  );
+ 
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+
+ 
+  evt.waitUntil(precache());
 });
 
-self.addEventListener('fetch', function(event) {
-  if (event.request.mode == 'navigate') {
-    console.log(`Handling fetch event for ${ event.request.url }`);
-    event.respondWith(
-      fetch(event.request).catch(function(exception) {
-        // The `catch` is only triggered if `fetch()` throws an exception,
-        // which most likely happens due to the server being unreachable.
-        console.error(
-          'Fetch failed; returning offline page instead.',
-          exception
-        );
-        return caches.open(OFFLINE_CACHE).then(function(cache) {
-          return cache.match(OFFLINE_URL);
-        });
-      })
-    );
-  } else {
-    // It’s not a request for an HTML document, but rather for a CSS or SVG
-    // file or whatever…
-     evt.respondWith(fromCache(evt.request));
-     evt.waitUntil(update(evt.request));
-  }
+ 
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
 
+ 
+  evt.respondWith(fromCache(evt.request));
+
+ 
+  evt.waitUntil(update(evt.request));
 });
 
+ 
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll([
+      './fonts/proximanova-bold-webfont.eot',
+      './fonts/proximanova-bold-webfont.ttf',
+      './fonts/proximanova-bold-webfont.woff',
+      './fonts/proximanova-light-webfont.eot',
+      './fonts/proximanova-light-webfont.ttf',
+      './fonts/proximanova-light-webfont.woff',
+      './fonts/proximanova-regular-webfont.eot',
+      './fonts/proximanova-regular-webfont.ttf',
+      './fonts/proximanova-regular-webfont.woff',
+      './index.html',
+      './js/nprogress.min.js',
+      './img/facebook-icon.png',
+      './img/autor.jpg',
+      './img/play.jpg',
+      './img/fifa08.jpg',
+      './img/potplayer.jpg',
+      './img/handycache.jpg',
+      './img/pes2015.jpg',
+      './js/app.js'
+    ]);
+  });
+}
+
+ 
 function fromCache(request) {
-  return caches.open(OFFLINE_CACHE).then(function (cache) {
+  return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
       return matching || Promise.reject('no-match');
     });
   });
 }
 
+ 
 function update(request) {
-  return caches.open(OFFLINE_CACHE).then(function (cache) {
+  return caches.open(CACHE).then(function (cache) {
     return fetch(request).then(function (response) {
       return cache.put(request, response);
     });
