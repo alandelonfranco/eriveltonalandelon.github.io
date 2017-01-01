f13ef381181ae2bb561060b7937d39b1caf59419
EriveltonAlandelon <eriveltonalandelon@gmail.com>
2017-01-01 03:11:16 -0200

    Testes

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,50 @@
-var files = [
-  "/css/estilo.css",
-  "/js/loadCSS.js",
-  "/js/cssrelpreload.js",
-  "/js/nprogress.min.js",
-  "/favicon.ico",
-  "/fonts/proximanova-bold-webfont.eot",
-  "/fonts/proximanova-bold-webfont.ttf",
-  "/fonts/proximanova-bold-webfont.woff",
-  "/fonts/proximanova-light-webfont.eot",
-  "/fonts/proximanova-light-webfont.ttf",
-  "/fonts/proximanova-light-webfont.woff",
-  "/fonts/proximanova-regular-webfont.eot",
-  "/fonts/proximanova-regular-webfont.ttf",
-  "/fonts/proximanova-regular-webfont.woff",
-  "/img/facebook-icon.png",
-  "/img/autor.jpg",
-  "/img/play.jpg",
-  "/img/fifa08.jpg",
-  "/img/potplayer.jpg",
-  "/img/handycache.jpg",
-  "/img/pes2015.jpg",
-  "/js/app.js",
-  "/js/app-reload.js"
-];
-// dev only
-/*
-if (typeof files == 'undefined') {
-  var files = [];
-} else {
-  files.push('./');
-}
-*/
+'use strict';
 
-var CACHE_NAME = 'erivelton';
+const PREFIX = 'erivelton';
+const HASH = '424c85f60'; // Calculated when running `gulp`.
+const OFFLINE_CACHE = `${ PREFIX }-${ HASH }`;
+const OFFLINE_URL = '/';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(OFFLINE_CACHE).then(function(cache) {
+      return cache.addAll([
+        '/css/estilo.css',
+        '/js/loadCSS.js',
+        '/js/cssrelpreload.js',
+        '/js/nprogress.min.js',
+        '/favicon.ico',
+        '/fonts/proximanova-bold-webfont.eot',
+        '/fonts/proximanova-bold-webfont.ttf',
+        '/fonts/proximanova-bold-webfont.woff',
+        '/fonts/proximanova-light-webfont.eot',
+        '/fonts/proximanova-light-webfont.ttf',
+        '/fonts/proximanova-light-webfont.woff',
+        '/fonts/proximanova-regular-webfont.eot',
+        '/fonts/proximanova-regular-webfont.ttf',
+        '/fonts/proximanova-regular-webfont.woff',
+        '/img/facebook-icon.png',
+        '/img/autor.jpg',
+        '/img/play.jpg',
+        '/img/fifa08.jpg',
+        '/img/potplayer.jpg',
+        '/img/handycache.jpg',
+        '/img/pes2015.jpg',
+        '/js/app.js',
+        '/js/app-reload.js'
+      ]);
+    })
+  );
+});
 
 self.addEventListener('activate', function(event) {
-  console.log('[SW] Activate');
+  // Delete old asset caches.
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function(keys) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (CACHE_NAME.indexOf(cacheName) == -1) {
-            console.log('[SW] Delete cache:', cacheName);
-            return caches.delete(cacheName);
+        keys.map(function(key) {
+          if (key != OFFLINE_CACHE) {
+            return caches.delete(key);
           }
         })
       );
@@ -50,29 +52,30 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-self.addEventListener('install', function(event){
-  console.log('[SW] Install');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return Promise.all(
-      	files.map(function(file){
-      		return cache.add(file);
-      	})
-      );
-    })
-  );
-});
-
 self.addEventListener('fetch', function(event) {
-  console.log('[SW] fetch ' + event.request.url)
-  event.respondWith(
-    caches.match(event.request).then(function(response){
-      return response || fetch(event.request.clone());
-    })
-  );
-});
+  if (event.request.mode == 'navigate') {
+    console.log(`Handling fetch event for ${ event.request.url }`);
+    event.respondWith(
+      fetch(event.request).catch(function(exception) {
+        // The `catch` is only triggered if `fetch()` throws an exception,
+        // which most likely happens due to the server being unreachable.
+        console.error(
+          'Fetch failed; returning offline page instead.',
+          exception
+        );
+        return caches.open(OFFLINE_CACHE).then(function(cache) {
+          return cache.match(OFFLINE_URL);
+        });
+      })
+    );
+  } else {
+    // It’s not a request for an HTML document, but rather for a CSS or SVG
+    // file or whatever…
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
 
-self.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event);
-  clients.openWindow('/');
 });
