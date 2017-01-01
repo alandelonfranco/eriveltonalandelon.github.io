d661472fa9cc7ea47201aa350ce15df733d74e84
EriveltonAlandelon <eriveltonalandelon@gmail.com>
2017-01-01 06:16:45 -0200

    Testes

--- service-worker.js
+++ service-worker.js
@@ -9,25 +9,25 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(OFFLINE_CACHE).then(function(cache) {
       return cache.addAll([
-        '/fonts/proximanova-bold-webfont.eot',
-        '/fonts/proximanova-bold-webfont.ttf',
-        '/fonts/proximanova-bold-webfont.woff',
-        '/fonts/proximanova-light-webfont.eot',
-        '/fonts/proximanova-light-webfont.ttf',
-        '/fonts/proximanova-light-webfont.woff',
-        '/fonts/proximanova-regular-webfont.eot',
-        '/fonts/proximanova-regular-webfont.ttf',
-        '/fonts/proximanova-regular-webfont.woff',
-        '/index.html',
-        '/js/nprogress.min.js',
-        '/img/facebook-icon.png',
-        '/img/autor.jpg',
-        '/img/play.jpg',
-        '/img/fifa08.jpg',
-        '/img/potplayer.jpg',
-        '/img/handycache.jpg',
-        '/img/pes2015.jpg',
-        '/js/app.js'
+        './fonts/proximanova-bold-webfont.eot',
+        './fonts/proximanova-bold-webfont.ttf',
+        './fonts/proximanova-bold-webfont.woff',
+        './fonts/proximanova-light-webfont.eot',
+        './fonts/proximanova-light-webfont.ttf',
+        './fonts/proximanova-light-webfont.woff',
+        './fonts/proximanova-regular-webfont.eot',
+        './fonts/proximanova-regular-webfont.ttf',
+        './fonts/proximanova-regular-webfont.woff',
+        './index.html',
+        './js/nprogress.min.js',
+        './img/facebook-icon.png',
+        './img/autor.jpg',
+        './img/play.jpg',
+        './img/fifa08.jpg',
+        './img/potplayer.jpg',
+        './img/handycache.jpg',
+        './img/pes2015.jpg',
+        './js/app.js'
       ]);
     })
   );
